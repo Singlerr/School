3006803e051de38842b3b2bdbd45e059abe1cd03
Singlerr <singlerr@naver.com>
2019-09-14 13:59:00 +0900

    First

---
+++ ArduinoGraph/ArduinoGraph/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ArduinoGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.geo = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.geo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // geo
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.geo.ChartAreas.Add(chartArea7);
+            this.geo.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend7.Name = "Legend1";
+            this.geo.Legends.Add(legend7);
+            this.geo.Location = new System.Drawing.Point(0, 0);
+            this.geo.Name = "geo";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.geo.Series.Add(series7);
+            this.geo.Size = new System.Drawing.Size(800, 450);
+            this.geo.TabIndex = 0;
+            this.geo.Text = "Geometry";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(229, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "....";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.geo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Geometry";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.geo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart geo;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ArduinoGraph/ArduinoGraph/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoGraph
+{
+    public partial class Form1 : Form
+    {
+        double anglePrev = 0;
+  
+        public static Form1 instance;
+        double x,y= 0;
+        static SerialPort port;
+        public Form1()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            geo.ChartAreas.Clear();
+            geo.Series.Clear();
+            geo.ChartAreas.Add("Draw");
+            geo.ChartAreas["Draw"].BackColor = Color.White;
+            geo.ChartAreas["Draw"].BackColor = Color.White;
+            geo.ChartAreas["Draw"].AxisX.Maximum = 20;
+            geo.ChartAreas["Draw"].AxisX.Minimum = 0;
+            geo.ChartAreas["Draw"].AxisX.Interval = 2;
+            geo.ChartAreas["Draw"].AxisX.MajorGrid.LineColor = Color.Black;
+            geo.ChartAreas["Draw"].AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            geo.ChartAreas["Draw"].AxisY.Maximum = 20;
+            geo.ChartAreas["Draw"].AxisY.Minimum = -20;
+            geo.ChartAreas["Draw"].AxisY.Interval = 2;
+            geo.ChartAreas["Draw"].AxisY.MajorGrid.LineColor = Color.Black;
+            geo.ChartAreas["Draw"].AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            geo.Series.Add("main");
+            geo.Series["main"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            geo.Series["main"].Color = Color.Blue;
+            geo.Series["main"].BorderWidth = 2;
+            geo.Series["main"].LegendText = "x";
+        }
+        public double func(double x)
+        {
+            return 7 * Math.Pow(x, 40) + 5 * Math.Pow(x, 6) + 2 * Math.Pow(x, 5) + 5 * Math.Pow(x, 6) + 5 * Math.Pow(x, 4) + 5;
+        }
+        public double f(double x,double xPrev, double h)
+        {
+            return (x - xPrev) / h;
+        }
+        public double Calculate(double x,double xPrev)
+        {
+            double timePrev = Current();
+            for (int i = 0; i < 1000000; i++)
+            {
+
+            }
+            double time = Current();
+            double t = (time - timePrev) / 1000;
+            return f(x, xPrev,t);
+
+        }
+     /*   protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            geo.ChartAreas.Clear();
+            geo.Series.Clear();
+            geo.ChartAreas.Add("Draw");
+            geo.ChartAreas["Draw"].BackColor = Color.White;
+            geo.ChartAreas["Draw"].BackColor = Color.White;
+            geo.ChartAreas["Draw"].AxisX.Maximum = 20;
+            geo.ChartAreas["Draw"].AxisX.Minimum = -20;
+            geo.ChartAreas["Draw"].AxisX.Interval = 2;
+            geo.ChartAreas["Draw"].AxisX.MajorGrid.LineColor = Color.Black;
+            geo.ChartAreas["Draw"].AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            geo.ChartAreas["Draw"].AxisY.Maximum = 20;
+            geo.ChartAreas["Draw"].AxisY.Minimum = -20;
+            geo.ChartAreas["Draw"].AxisY.Interval = 2;
+            geo.ChartAreas["Draw"].AxisY.MajorGrid.LineColor = Color.Black;
+            geo.ChartAreas["Draw"].AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            geo.Series.Add("main");
+            geo.Series["main"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            geo.Series["main"].Color = Color.Blue;
+            geo.Series["main"].BorderWidth = 2;
+            geo.Series["main"].LegendText = "x";
+         
+            x = -20;
+            timer1.Start();
+
+              for (double x = -20;x<20;x += 0.1)
+                {
+                    
+                    geo.Series["main"].Points.AddXY(x, Calculate(x));
+                  
+                }
+           
+            
+        }*/
+        public void Draw(double y)
+        {
+            // geo.ChartAreas.Clear();
+            //  geo.Series.Clear();
+            //  geo.ChartAreas.Add("Draw");
+            if (-20 < x)
+            {
+                geo.ChartAreas["Draw"].AxisX.Maximum = 20+x;
+                geo.ChartAreas["Draw"].AxisX.Minimum = -20+x;
+                geo.Series["main"].Points.AddXY(x, y);
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            x = -20;
+            timer1.Start();
+        }
+        public static double Current()
+        {
+            DateTime Jan1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan javaSpan = DateTime.UtcNow - Jan1970;
+            return javaSpan.TotalMilliseconds;
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+           
+        }
+        public void Set(String s)
+        {
+            label1.Text = s;
+        }
+        private void DataReceived(object sender,SerialDataReceivedEventArgs e)
+        {
+            double angle = 0;
+            bool b = double.TryParse(port.ReadExisting(), out angle);
+            if (b)
+            {
+                y = Calculate(angle, anglePrev) / 100;
+                instance.BeginInvoke(new Action(delegate ()
+                {
+                    Set(y.ToString());
+                }));
+                if (y >= -20 && y <= 20)
+                {
+                    instance.BeginInvoke(new Action(delegate ()
+                    {
+                        Draw(y);
+                    }));
+                }
+                anglePrev = angle;
+                x += 0.1;
+            }
+        }
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            port = new SerialPort();
+            port.PortName = textBox1.Text;
+            port.BaudRate = 115200;
+            port.Open();
+            /* System.Threading.Thread t = new System.Threading.Thread(delegate ()
+             {
+                 while (true)
+                 {
+
+                     double angle = 0;
+                     instance.BeginInvoke(new Action(delegate()
+                     {
+                         Draw(Calculate(angle, anglePrev));
+                     }));
+
+                     anglePrev = angle;
+                     x += 0.1;
+                 }
+             });
+             t.Start();*/
+            timer2.Interval = 10;
+            port.DataReceived += DataReceived;
+        //    timer2.Start();
+        }
+       
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            double angle = 0;
+            bool b = double.TryParse(port.ReadExisting(), out angle);
+            if (b)
+            {
+                y = Calculate(angle, anglePrev);
+                if (y >= -20 && y <= 20)
+                {
+                    Draw(y);
+                }
+                anglePrev = angle;
+                x += 0.1;
+            }
+        }
+    }
+}
